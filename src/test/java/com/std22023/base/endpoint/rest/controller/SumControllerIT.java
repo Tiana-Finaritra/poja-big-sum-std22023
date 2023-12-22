cb830e71118c713944c4b07fefaf99ffcf702447
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2023-12-22 09:27:31 +0300

    test

--- src/test/java/com/std22023/base/endpoint/rest/controller/SumControllerIT.java
+++ src/test/java/com/std22023/base/endpoint/rest/controller/SumControllerIT.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 import java.math.BigInteger;
 
 @SpringBootTest
@@ -18,6 +20,7 @@ class SumControllerTest {
     public SumControllerTest(MockMvc mockMvc) {
         this.mockMvc = mockMvc;
     }
+
 
     @Test
     void testBigSum() throws Exception {
